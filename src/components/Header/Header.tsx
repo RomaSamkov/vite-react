6f5e13d527cc3273532daf078a316af392a66e1f
RomaSamkov <Romansamkov.rs@gmail.com>
2023-07-07 15:49:54 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,46 @@
+import "./Header.scss";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-container">
+      <div className="container">
+        <div className="header-info">
+          <a href="" className="chat">
+            Chat with us
+          </a>
+          <a href="" className="phone">
+            +12345678
+          </a>
+          <a href="" className="email">
+            info@freshnesecom.com
+          </a>
+        </div>
+        <div className="header-links">
+          <div className="link">Blog</div>
+          <div className="link">Abous Us</div>
+          <div className="link">Carrers</div>
+        </div>
+      </div>
+      <div className="header-default">
+        <div className="logo">Freshnesecom</div>
+        <div className="search">
+          <input
+            type="text"
+            className="header-input"
+            placeholder="Search Products, categories ... "
+          />
+        </div>
+        <div className="user-info">
+          <div className="user">
+            <img src="src/assets/svg/ic-actions-user.png" alt="" />
+          </div>
+          <div className="cart">
+            <img src="src/assets/svg/ic-ecommerce-basket.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
