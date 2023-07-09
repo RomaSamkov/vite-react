8ff2e9f91c1c60f5f8a188976784c6a0fc9bdbc7
RomaSamkov <Romansamkov.rs@gmail.com>
2023-07-09 12:14:11 +0300

    Create HomePage.tsx

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
+};
+
+export default HomePage;
