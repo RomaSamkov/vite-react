3168119ba07fe9f162c2e6ce9e388baf3e08a9b3
RomaSamkov <Romansamkov.rs@gmail.com>
2023-07-09 12:10:06 +0300

    add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Header from "./components/Header/Header";
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Products from "./components/Products/Products";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Header />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<Products />} />
       </Routes>
       {/* <Header /> */}
